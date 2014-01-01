f4494990da138eb5f62000f0ad53d2d6ea944538
Nathan Witmer <nathan@zerowidth.com>
2014-01-01 14:28:42 -0700

    Draw things with requestAnimationFrame

--- src/sketch/core.cljs
+++ src/sketch/core.cljs
@@ -2,12 +2,30 @@
   (:require [clojure.browser.dom :as dom])
   (:require-macros [sketch.macros :as debug]))
 
-(defn init []
-  (let [canvas (dom/get-element "sketch")
-        ctx    (.getContext canvas "2d")]
-    (dom/set-properties canvas {"width" "400px" "height" "400px"})
-    (doto ctx
-      (aset "fillStyle" "#F00")
-      (.fillRect 100 100 200 100))))
+(def canvas-width 400)
+(def canvas-height 400)
 
-(init)
+(defn setup [canvas ctx]
+  (let [width (str canvas-width "px")
+        height (str canvas-height "px")]
+    (dom/set-properties canvas {"width" width "height" height}))
+  ; invert the y coordinates so y starts at bottom
+  (.setTransform ctx 1 0 0 -1 0 canvas-height))
+
+(defn update [dt])
+
+(defn draw [ctx]
+  (doto ctx
+    (aset "fillStyle" "#F00")
+    (.fillRect 100 100 200 100)))
+
+(defn init [canvas-id]
+  (let [canvas (dom/get-element canvas-id)
+        ctx    (.getContext canvas "2d")]
+    (setup canvas ctx)
+    (letfn [(frame [dt]
+              (draw ctx)
+              (js/requestAnimationFrame frame))]
+      (js/requestAnimationFrame frame))))
+
+(init "sketch")

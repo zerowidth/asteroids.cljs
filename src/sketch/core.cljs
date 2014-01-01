248eb964eefdc79a54d34ef7fc1d031635e486b2
Nathan Witmer <nathan@zerowidth.com>
2014-01-01 14:08:25 -0700

    Draw something on a canvas

--- src/sketch/core.cljs
+++ src/sketch/core.cljs
@@ -1,4 +1,13 @@
 (ns sketch.core
+  (:require [clojure.browser.dom :as dom])
   (:require-macros [sketch.macros :as debug]))
 
-(. js/console (log "Hello world!"))
+(defn init []
+  (let [canvas (dom/get-element "sketch")
+        ctx    (.getContext canvas "2d")]
+    (dom/set-properties canvas {"width" "400px" "height" "400px"})
+    (doto ctx
+      (aset "fillStyle" "#F00")
+      (.fillRect 100 100 200 100))))
+
+(init)
--- src/sketch/macros.clj
+++ src/sketch/macros.clj
@@ -1,4 +1,6 @@
 (ns sketch.macros)
 
 (defmacro p [& more]
-  `(.apply (.-log js/console) js/console (into-array (map pr-str '~more ))))
+  `(.log js/console ~@more))
+(defmacro pp [& more]
+  `(.apply (.-log js/console) js/console (into-array (map pr-str '~more))))

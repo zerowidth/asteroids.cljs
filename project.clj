384727a5909875e163b19474157ca0ed8dbde220
Nathan Witmer <nathan@zerowidth.com>
2014-01-12 08:53:53 -0700

    Add clojurescript.test for testing

--- project.clj
+++ project.clj
@@ -5,15 +5,23 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2127"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.1"]
+            [com.cemerick/clojurescript.test "0.2.1"]]
 
   :source-paths ["src"]
 
   :cljsbuild {
-    :builds [{:id "sketch"
-              :source-paths ["src"]
-              :compiler {
-                :output-to "public/sketch.js"
-                :output-dir "public/js"
-                :optimizations :none
-                :source-map true}}]})
+              :builds [{:id "sketch"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :output-to "public/sketch.js"
+                                   :output-dir "public/js"
+                                   :optimizations :none
+                                   :source-map true}}
+                       {:id "test"
+                        :source-paths ["src" "test"]
+                        :compiler {:output-to "target/cljs/test.js"
+                                   :optimizations :whitespace}}]
+              :test-commands {"phantom" ["phantomjs" :runner
+                                         "window.literal_js_was_evaluated=true"
+                                         "target/cljs/test.js"]}})
---
+++ test/sketch/test/physics.cljs
@@ -0,0 +1,7 @@
+(ns sketch.test.physics
+  (:require-macros [cemerick.cljs.test
+                    :refer (is deftest with-test run-tests testing test-var)])
+  (:require [cemerick.cljs.test :as t]))
+
+(deftest test-physical-properties
+  )

f4d16861d205bb78d549df9d1c0686523cedd5ac
Nathan Witmer <nathan@zerowidth.com>
2014-01-12 10:18:43 -0700

    Calculate inverse mass based on area and density

--- src/sketch/physics.cljs
+++ src/sketch/physics.cljs
@@ -2,10 +2,30 @@
   (:require [sketch.vector :as v]
             [sketch.rotation :as r]))
 
-(defn integrate [dt {:keys [position velocity
-                            orientation angular-velocity] :as body}]
+(defn polygonal-area
+  "Use the shoelace (surveyor's) formula for calculating the signed area of a polygon"
+  [vertices]
+  (let [pairs (partition 2 1 (concat vertices (list (first vertices))))
+        signed-area (fn [ [[x0 y0] [x1 y1]] ] (- (* x0 y1) (* x1 y0)))]
+    (/ (reduce + (map signed-area pairs)) 2)))
+
+(defn physical-properties
+  "Calculate the physical properties (area, mass, angular moment) of a body."
+  [{:keys [offsets density] :as body}]
+  (let [area (polygonal-area offsets)
+        mass (* density area)
+        inv-mass (if (> mass 0) (/ mass) 0)]
+    (merge-with (fn [a b] a)
+                body
+                {:area area :inverse-mass inv-mass})))
+
+(defn integrate
+  "update the position and orientation of a body given a time delta dt"
+  ; TODO handle acceleration too
+  [dt {:keys [position velocity
+              orientation angular-velocity] :as body}]
   (let [new-position (v/+ position (v/* velocity dt))
         new-orientation (r/add-angle orientation (* angular-velocity dt))]
-    (assoc body :position new-position
+    (assoc body
+           :position new-position
            :orientation new-orientation)))
-
--- test/sketch/test/physics.cljs
+++ test/sketch/test/physics.cljs
@@ -1,14 +1,35 @@
 (ns sketch.test.physics
-  (:require [purnam.cljs])
+  (:require [purnam.cljs]
+            [sketch.physics :as p])
   (:use-macros [purnam.test :only [init describe it is is-not]]))
 
 (init) ; initialize jasmine
 
-(deftest test-physical-properties
-  )
+(def triangle-offsets [[0 0] [10 0] [0 20]])
+(def square-offsets [[0 0] [10 0] [10 10] [0 10]])
+(def polygon-offsets [[0 0] [10 0] [10 10] [0 20]])
+(def test-square
+  {:density 2
+   :offsets square-offsets})
 
 (describe
-  {:doc "truth"
-   :globals [value true]}
-  (it "is true"
-      (is value true)))
+  {:doc "polygonal-area"}
+  (it "calculates the area of a square"
+      (is (p/polygonal-area square-offsets) 100))
+  (it "calculates the area of a triangle"
+      (is (p/polygonal-area triangle-offsets) 100))
+  (it "calculates the area of a polygon"
+      (is (p/polygonal-area polygon-offsets) 150)))
+
+(describe
+  {:doc "physical-properties with a square"
+   :globals [square (p/physical-properties test-square)]}
+  (it "calculates the area"
+      (is (:area square) 100))
+  (it "sets the inverse mass based on area and density"
+      (is (:inverse-mass square) 0.005))
+  (it "does not override inverse mass if set"
+      (is
+        (:inverse-mass (p/physical-properties
+                         (assoc test-square :inverse-mass 0.1)))
+        0.1)))

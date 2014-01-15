a6c7220d939f2df36ef8df9ae2fc9fa070fb7465
Nathan Witmer <nathan@zerowidth.com>
2014-01-14 20:25:24 -0700

    Add macro for jasmine toBeCloseTo matcher

--- src/sketch/test.clj
+++ src/sketch/test.clj
@@ -2,3 +2,6 @@
 
 (defmacro is-near [value expected]
   (list '.toBeNear (list 'js/expect value) expected (str value)))
+
+(defmacro is-within-delta [value expected]
+  (list '.toBeCloseTo (list 'js/expect value) expected 3))

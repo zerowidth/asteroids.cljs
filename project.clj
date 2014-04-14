e7c7ecc614caf2b8e26240ac0887e0b9818e0057
Nathan Witmer <nathan@zerowidth.com>
2014-04-13 20:29:38 -0600

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2127"]
+                 [org.clojure/clojurescript "0.0-2202"]
                  [im.chit/purnam "0.1.8"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]
+  :plugins [[lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.2.1"]]
 
   :source-paths ["src"]
